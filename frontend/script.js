d75acda34d225bed5d4e23a83f3dfc07ab2fb380
Thilak <ncas2527psd2285@gmail.com>
2026-05-14 15:24:01 +0530

    registration app

---
+++ frontend/script.js
@@ -0,0 +1,27 @@
+async function register() {
+    const data = {
+        fullname: document.getElementById("fullname").value,
+        dob: document.getElementById("dob").value,
+        gender: document.getElementById("gender").value,
+        marital_status: document.getElementById("marital_status").value,
+        email: document.getElementById("email").value,
+        phone: document.getElementById("phone").value,
+        address: document.getElementById("address").value,
+        city: document.getElementById("city").value,
+        state: document.getElementById("state").value,
+        qualification: document.getElementById("qualification").value,
+        occupation: document.getElementById("occupation").value,
+        age: document.getElementById("age").value,
+        password: document.getElementById("password").value
+    };
+
+    await fetch("http://localhost:5000/register", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    });
+
+    alert("Registered Successfully");
+}
